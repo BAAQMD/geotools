6d94fc7cec6ad1520a26431af729d6db69858021
David Holstius <david.holstius@gmail.com>
2019-09-12 13:50:59 -0700

    typo

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
   st_sample_msg <-
     str_c(
       "-------------------------------------------------------------------------------",
-      "A better st_sample() is now provided by the `sf` package.",
+      "A better `st_sample()` is now provided by the `sf` package.",
       "Its behavior is different, but more well-supported and future-proofed. ",
       "If you really want to use the old `geotools::st_sample()`, ",
       "then please invoke `st_sample_geotools()` instead.",
