6f1f965edf2fdbf0ff791d352910df81a59d32be
David Holstius <david.holstius@gmail.com>
2019-12-06 11:15:26 -0800

    pull `leaflet` functionality out into new `lltools` package

--- R/addSolidPolygons.R
+++
@@ -1,3 +0,0 @@
-addSolidPolygons <- function (leaflet_map, color, opacity, fillOpacity = opacity, stroke = FALSE, ...) {
-  addPolygons(leaflet_map, fillColor = color, color = color, stroke = stroke, fillOpacity = fillOpacity, ...)
-}
