788c6db340c6b23bada598596f806431ea8b86f9
David Holstius <david.holstius@gmail.com>
2020-08-03 11:12:25 -0700

    document as_raster()

--- R/as_raster.R
+++ R/as_raster.R
@@ -1,5 +1,37 @@
+#' as_raster
+#'
+#' Rasterize geodata
+#'
+#' @param x passed to `as_gridded()`
+#' @param layer layer name
+#' @param coord_vars names of coordinates (optional)
+#' @param crs passed to `as_gridded()`
+#' @param ... passes to `raster::raster()`
+#'
+#' @importFrom raster raster
+#'
 #' @export
-as_raster <- function (x, layer, coord_vars = NULL, crs = NULL, ...) {
-  grd <- as_gridded(x, layer = layer, coord_vars = coord_vars, crs = crs)
-  raster::raster(grd, layer = layer, ...)
+as_raster <- function (
+  x,
+  layer,
+  coord_vars = NULL,
+  crs = NULL,
+  ...
+) {
+
+  gridded_geodata <-
+    as_gridded(
+      x,
+      layer = layer,
+      coord_vars = coord_vars,
+      crs = crs)
+
+  raster_object <-
+    raster::raster(
+      gridded_geodata,
+      layer = layer,
+      ...)
+
+  return(raster_object)
+
 }
