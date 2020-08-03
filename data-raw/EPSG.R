080e3335279848dad79a8fe4a833528145662206
David Holstius <david.holstius@gmail.com>
2020-08-03 11:07:16 -0700

    remove saved datum .rda files and generate on-the-fly with data/datums.R instead

--- data-raw/EPSG.R
+++
@@ -1,7 +0,0 @@
-NAD83_UTM10 <- 26910
-WGS84_GPS <- 4326
-
-use_data(
-  NAD83_UTM10,
-  WGS84_GPS,
-  overwrite = TRUE)
---
+++ data/datums.R
@@ -0,0 +1,31 @@
+#
+# Note: see `geotools-package.R` for associated documentation.
+#
+
+NAD83_UTM10 <- 26910
+
+UTM10_NAD83 <-
+  sp::CRS("+init=epsg:26910")
+
+UTM10_NAD27 <-
+  sp::CRS("+proj=utm +zone=10 +datum=NAD27 +ellps=clrk66 +nadgrids=@conus,@alaska,@ntv2_0.gsb,@ntv1_can.da")
+
+WGS84_GPS <- 4326
+
+WGS84 <-
+  sp::CRS("+init=epsg:4326")
+
+EPSG_4326 <-
+  sp::CRS("+init=epsg:4326")
+
+EPSG_3310 <-
+  sp::CRS("+init=epsg:3310")
+
+EPSG_32610 <-
+  sp::CRS("+init=epsg:32610")
+
+UTM10 <-
+  sp::CRS("+init=epsg:32610")
+
+CA_ALBERS_NAD83 <-
+  sp::CRS("+init=epsg:3310")
