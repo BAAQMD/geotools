5a138a26d0b460d5655fc1026f5a05863022b120
David Holstius <david.holstius@gmail.com>
2020-08-03 11:12:35 -0700

    document as_gridded()

--- R/as_gridded.R
+++ R/as_gridded.R
@@ -1,17 +1,33 @@
+#' as_gridded
+#'
+#' @param x `sf` or `Spatial*` object
+#' @param layer name of layer to grid
+#' @param coord_vars names of coordinate variables
+#' @param crs suitable for use with `proj4string(.) <- crs`
+#'
+#' @importFrom sf st_crs st_drop_geometry st_centroid st_coordinates
+#' @importFrom sp gridded proj4string
+#' @importFrom dplyr mutate_at
+#'
 #' @export
-as_gridded <- function (x, layer, coord_vars = NULL, crs = NULL) {
+as_gridded <- function (
+  x,
+  layer,
+  coord_vars = NULL,
+  crs = NULL
+) {
 
   if (inherits(x, "sf")) {
 
-    crs <- st_crs(x)[["proj4string"]]
+    crs <- sf::st_crs(x)[["proj4string"]]
 
     if (is.null(coord_vars)) {
-      centroid_geodata <- st_centroid(x)
-      coord_vars <- colnames(st_coordinates(centroid_geodata))[1:2]
-      x <- drop_geometry(coordinates_to_columns(centroid_geodata, coords = coord_vars))
+      centroid_geodata <- sf::st_centroid(x)
+      coord_vars <- colnames(sf::st_coordinates(centroid_geodata))[1:2]
+      x <- sf::st_drop_geometry(coordinates_to_columns(centroid_geodata, coords = coord_vars))
 
       # HACK
-      x <- mutate_at(x, coord_vars, round)
+      x <- dplyr::mutate_at(x, coord_vars, round)
 
     }
 
