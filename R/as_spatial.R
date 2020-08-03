cf31260eeb829b69812cac5ec9a0656da8f6a050
David Holstius <david.holstius@gmail.com>
2020-08-03 11:12:18 -0700

    document as_spatial()

--- R/as_spatial.R
+++ R/as_spatial.R
@@ -1,19 +1,52 @@
+#' as_spatial
+#'
 #' Create a SpatialPointsDataFrame from tabular data
 #'
-#' @importFrom dplyr select select_vars
+#' @param input_data tabular data
+#' @param coord_vars names of variables in `input_data` that contain coordinate values
+#' @param crs as would be returned by `CRS()`
+#' @param na.rm drop any rows with non-finite coordinates
+#' @param ... passed to `sp::SpatialPointsDataFrame()`
+#' @param verbose display messages
+#'
+#' @importFrom dplyr select select_vars filter_at
 #' @importFrom sp SpatialPointsDataFrame
+#'
 #' @export
-as_spatial <- function (input_data, coord_vars, crs = CRS(NA_character_), na.rm = TRUE, ...) {
+as_spatial <- function (
+  input_data,
+  coord_vars,
+  crs = CRS(NA_character_),
+  na.rm = TRUE,
+  ...,
+  verbose = getOption("verbose")
+) {
 
   msg <- function (...) if (isTRUE(verbose)) message("[as_spatial] ", ...)
 
-  coord_vars <- dplyr::select_vars(names(input_data), coord_vars)
-  coord_data <- dplyr::select(input_data, coord_vars)
+  coord_vars <-
+    dplyr::select_vars(
+      names(input_data),
+      coord_vars)
+
+  coord_data <-
+    dplyr::select(
+      input_data,
+      coord_vars)
 
   if (isTRUE(na.rm)) {
 
-    coord_data <- filter_at(coord_data, vars(coord_vars), all_vars(is.finite(.)))
-    attr_data <- filter_at(input_data, vars(coord_vars), all_vars(is.finite(.)))
+    coord_data <-
+      dplyr::filter_at(
+        coord_data,
+        vars(coord_vars),
+        all_vars(is.finite(.)))
+
+    attr_data <-
+      dplyr::filter_at(
+        input_data,
+        vars(coord_vars),
+        all_vars(is.finite(.)))
 
   } else {
 
@@ -23,7 +56,10 @@ as_spatial <- function (input_data, coord_vars, crs = CRS(NA_character_), na.rm 
   }
 
   geodata <-
-    sp::SpatialPointsDataFrame(coord_data, attr_data, ...) %>%
+    sp::SpatialPointsDataFrame(
+      coord_data,
+      attr_data,
+      ...) %>%
     set_CRS(crs)
 
   coordnames(geodata) <-
