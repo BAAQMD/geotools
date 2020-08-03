d67466970231a9daf7ebedf0bb8d6e2fac5ac189
David Holstius <david.holstius@gmail.com>
2020-08-03 11:12:03 -0700

    Update gContainsOrOverlaps.R

--- R/gContainsOrOverlaps.R
+++ R/gContainsOrOverlaps.R
@@ -1,15 +1,21 @@
 #' gContainsOrOverlaps
 #'
-#' @description Default basis for `gFilter` is `gContainsOrOverlaps`
+#' This is the default predicate used by `gFilter()`.
+#'
+#' @param geom2 `Spatial*` object
+#' @param geom1 `Spatial*` object
+#' @param byid passed to `rgeos::gContains()` and `rgeos::gOverlaps()`
+#'
 #' @note Unlike most `rgeos` functions, this returns row.names !!
 #'
-#' @importFrom purrr is_empty
+#' @importFrom rlang is_empty
+#' @importFrom rgeos gContains gOverlaps
 #'
 #' @export
 gContainsOrOverlaps <- function (geom2, geom1, byid = TRUE) {
 
   # First pass: test for *containment*
-  ij1 <- gContains(geom2, geom1, byid = byid)
+  ij1 <- rgeos::gContains(geom2, geom1, byid = byid)
   is_contained <- as.logical(rowSums(ij1))
 
   # Preliminary result: may be modified by second pass (below)
@@ -18,8 +24,10 @@ gContainsOrOverlaps <- function (geom2, geom1, byid = TRUE) {
   # Second pass: test for *overlap* --- but only consider features
   # not already passing the first test (i.e., containment)
   remainder <- geom1[which(!is_contained), ]
-  if (!purrr::is_empty(remainder)) {
-    ij2 <- gOverlaps(geom2, remainder, byid = byid)
+  if (rlang::is_empty(remainder)) {
+    # do nothing
+  } else {
+    ij2 <- rgeos::gOverlaps(geom2, remainder, byid = byid)
     is_overlapping <- as.logical(rowSums(ij2))
     result <- union(result, row.names(remainder[is_overlapping]))
   }
