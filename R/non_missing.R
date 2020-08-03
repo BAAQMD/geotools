fa8a7779a0c88a215703db1258f4f92b65699768
David Holstius <david.holstius@gmail.com>
2020-08-03 11:09:14 -0700

    document non_missing()

--- R/non_missing.R
+++ R/non_missing.R
@@ -1,8 +1,11 @@
 #' Return all rows of a Spatial*DataFrame object except those with NA in the given column
 #'
+#' @param spdf `Spatial*DataFrame` object
+#' @param variable name of column
+#'
 #' @export
 non_missing <- function (spdf, variable) {
   stopifnot(inherits(spdf, "Spatial"))
   stopifnot("data" %in% slotNames(spdf))
-  spdf[!is.na(spdf@data[,variable]),]
+  return(spdf[!is.na(spdf@data[,variable]),])
 }
