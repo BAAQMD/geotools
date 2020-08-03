30138053da95c18118770ce355ac5c247da7e985
David Holstius <david.holstius@gmail.com>
2020-08-03 11:12:53 -0700

    Update defunct.R

--- R/defunct.R
+++ R/defunct.R
@@ -1,9 +1,14 @@
-#' @param spobj
+#' sp_centroid
 #'
-#' @param ...
+#' Extract the coordinates, as a simple vector, of the centroid of a Spatial* object.
+#'
+#' @param spobj `Spatial*` object
+#' @param ... passed to `gCentroid()`
+#'
+#' @importFrom rgeos gCentroid
 #'
 #' @export
 sp_centroid <- function (spobj, ...) {
   .Defunct(msg = "Try sf::st_centroid() or sp::gCentroid() instead.")
-  spobj %>% gCentroid(...) %>% coordinates %>% as.vector
+  spobj %>% rgeos::gCentroid(...) %>% coordinates %>% as.vector
 }
