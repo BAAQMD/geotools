72f50f8332cd697dc2601fe5535b42f12a59aff8
David Holstius <david.holstius@gmail.com>
2020-08-03 11:10:52 -0700

    document and improve st_sample_points()

--- R/st_sample_points.R
+++ R/st_sample_points.R
@@ -4,25 +4,37 @@
 #'
 #' @param geodata (sf) most likely polygons or polylines (inside or along which to sample)
 #' @param size (int) number of points to sample (in or along each feature)
+#' @param verbose display messages
 #'
 #' @return `sfc` of POINTS, joined back to the original `geodata` (i.e., an overlay)
 #'
 #' @export
-st_sample_points <- function (geodata, size, verbose = getOption("verbose")) {
+st_sample_points <- function (
+  geodata,
+  size,
+  verbose = getOption("verbose")
+) {
 
   msg <- function (...) if(isTRUE(verbose)) message("[st_sample_points] ", ...)
 
-  suppressPackageStartupMessages({
-    require(lazyeval)
-    require(rlang)
-  })
+  size <-
+    rlang::enquo(size)
 
-  size <- enquo(size)
-  sample_sizes <- round(eval_tidy(size, geodata))
-  stopifnot(all_true(is.finite(sample_sizes)), all_true(sample_sizes >= 0))
+  sample_sizes <-
+    round(
+      rlang::eval_tidy(size, geodata))
+
+  stopifnot(
+    funtools::all_true(
+      is.finite(sample_sizes)),
+    funtools::all_true(
+      sample_sizes >= 0))
 
   msg("projecting to planar CRS (NAD83_UTM10)")
-  planar_geodata <- st_transform(geodata, NAD83_UTM10)
+  planar_geodata <-
+    sf::st_transform(
+      geodata,
+      crs = NAD83_UTM10)
 
   msg("sampling n = ", sum(sample_sizes), " points over ", nrow(geodata), " features")
   planar_points <-
@@ -32,11 +44,18 @@ st_sample_points <- function (geodata, size, verbose = getOption("verbose")) {
     st_sf()
 
   msg("joining points back to features")
-  joined <-
-    planar_points %>%
-    st_join(planar_geodata, join = st_intersects)
+  joined_geodata <-
+    sf::st_join(
+      planar_points,
+      planar_geodata,
+      join = st_intersects)
 
   msg("reprojecting back to original CRS (", st_crs(geodata), ")")
-  return(st_transform(joined, st_crs(geodata)))
+  reprojected_geodata <-
+    sf::st_transform(
+      joined_geodata,
+      sf::st_crs(geodata))
+
+  return(reprojected_geodata)
 
 }
