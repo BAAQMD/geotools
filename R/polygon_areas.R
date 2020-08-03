00b2814accaeb5c569d526896e3ff72caaba24b3
David Holstius <david.holstius@gmail.com>
2020-08-03 11:13:41 -0700

    document polygon_areas()

--- R/polygon_areas.R
+++ R/polygon_areas.R
@@ -1,5 +1,7 @@
 #' Calculate the area of each polygon in a SpatialPolygons* object
 #'
+#' @param spdf `SpatialPolygons*` object
+#'
 #' @export
 polygon_areas <- function (spdf) {
   sapply(spdf@polygons, function(x) x@area / 1e6)
