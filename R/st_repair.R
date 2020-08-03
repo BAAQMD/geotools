e237f41da50f9830abecb4a3cf7513d7ffcb4e62
David Holstius <david.holstius@gmail.com>
2020-08-03 11:11:24 -0700

    document st_repair()

--- R/st_repair.R
+++ R/st_repair.R
@@ -1,14 +1,45 @@
+#' st_repair
+#'
+#' Attempt to repair problems in geodata.
+#'
+#' @param geodata `sf` object
+#' @param datum passed to `sf::st_transform()`
+#' @param view (optional) show result using `mapview()`
+#' @param verbose display messages
+#'
+#' @importFrom sf st_transform st_buffer st_union
+#' @importFrom mapview mapview
+#'
 #' @export
-st_repair <- function (geodata, datum = st_crs(32610), view = FALSE, verbose = TRUE) {
+st_repair <- function (
+  geodata,
+  datum = st_crs(32610),
+  view = FALSE,
+  verbose = TRUE
+) {
 
   msg <- function (...) if(isTRUE(verbose)) message("[st_repair] ", ...)
   msg("repairing n = ", nrow(geodata))
 
-  projected <- st_transform(geodata, datum)
-  buffered <- st_buffer(projected, 0)
-  unioned <- st_union(buffered, by_feature = TRUE)
+  projected <-
+    sf::st_transform(
+      geodata,
+      crs = datum)
 
-  repaired <- st_filter_valid(unioned) %>% st_transform(st_crs(geodata))
+  buffered <-
+    sf::st_buffer(
+      projected,
+      dist = 0)
+
+  unioned <-
+    sf::st_union(
+      buffered,
+      by_feature = TRUE)
+
+  repaired <-
+    st_transform(
+      st_filter_valid(unioned),
+      st_crs(geodata))
 
   if (isTRUE(view)) mapview::mapview(repaired)
 
