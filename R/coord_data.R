ca090a2620e440bcc1c99beffbd11a2c58a0748c
David Holstius <david.holstius@gmail.com>
2020-08-03 11:12:47 -0700

    improve coord_data()

--- R/coord_data.R
+++ R/coord_data.R
@@ -1,6 +1,19 @@
-#' Return the coordinates of a Spatial* object as a data.frame
+#' Return the coordinates of a Spatial* object as a tibble
+#'
+#' @param spobj anything for which `coordinates(.)` works
 #'
 #' @export
 coord_data <- function (spobj) {
-  data.frame(coordinates(spobj), row.names=row.names(spobj))
+
+  coord_df <-
+    data.frame(
+      coordinates(spobj),
+      row.names = row.names(spobj))
+
+  coord_tbl <-
+    tibble::as_tibble(
+      coord_df)
+
+  return(coord_tbl)
+
 }
