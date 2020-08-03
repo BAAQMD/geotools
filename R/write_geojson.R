3895418c7c4ed95441be4a5a193f95d8b3759d0e
David Holstius <david.holstius@gmail.com>
2020-08-03 11:10:28 -0700

    tidy and document write_geojson

--- R/write_geojson.R
+++ R/write_geojson.R
@@ -1,3 +1,19 @@
+#' write_geojson
+#'
+#' Write a layer to a GeoJSON file.
+#'
+#' @param spobj geodata
+#' @param dsn output path; defaults to the current working directory
+#' @param layer name of layer to write
+#' @param pretty passed to `jsonlite::toJSON()`
+#' @param digits passed to `jsonlite::toJSON()`
+#' @param ... passed to `rgdal::writeOGR()`
+#' @param overwrite_layer passed to `rgdal::writeOGR()`
+#' @param verbose display messages
+#'
+#' @importFrom rgdal writeOGR
+#' @importFrom jsonlite fromJSON toJSON
+#'
 #' @export
 write_geojson <- function (
   spobj,
@@ -13,13 +29,13 @@ write_geojson <- function (
   outfile <- file.path(dsn, str_c(layer, ".geojson"))
   if (verbose) message("Writing to ", outfile)
 
-  require(rgdal)
-  require(jsonlite)
-
   # Use rgdal's GeoJSON driver to write to a tempfile
   tmp_file <- tempfile()
-  success <- writeOGR(
-    spobj, dsn = tmp_file, layer = "", ...,
+  success <- rgdal::writeOGR(
+    spobj,
+    dsn = tmp_file,
+    layer = "",
+    ...,
     driver = "GeoJSON",
     overwrite_layer = overwrite_layer)
 
