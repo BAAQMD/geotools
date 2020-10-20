1f2fea2cc43a2c8ce4708c8dd9e99a5a2e44735c
David Holstius <david.holstius@gmail.com>
2020-10-20 15:08:02 -0700

    add bind_rasters()

---
+++ R/bind_rasters.R
@@ -0,0 +1,46 @@
+#' bind_rasters
+#'
+#' @param x
+#' @param ...
+#' @param fill
+#' @param outfile
+#' @param verbose
+#'
+#' @return
+#' @export
+bind_rasters <- function (
+  x,
+  ...,
+  fill = NA,
+  outfile = NULL,
+  verbose = getOption("verbose", default = FALSE)
+) {
+
+  msg <- function (...) if(isTRUE(verbose)) message("[bind_rasters] ", ...)
+
+  raster_list <-
+    append(x, list(...))
+
+  names(raster_list) <-
+    map(raster_list, names)
+
+  extent_list <-
+    map(raster_list, raster::extent)
+
+  merged_extent <-
+    raster::extent(reduce(raster_list, raster::merge))
+
+  expanded_list <-
+    map(raster_list, raster::extend, merged_extent)
+
+  stacked <-
+    reduce(expanded_list, raster::addLayer)
+
+  if (isFALSE(is.null(outfile))) {
+    msg("writing to: ", outfile)
+    raster::writeRaster(stacked, filename = outfile, overwrite = TRUE)
+  }
+
+  return(stacked)
+
+}
