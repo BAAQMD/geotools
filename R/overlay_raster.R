58f4f7afbc7ccc26eaa4c1fe17872e9a80dbefdd
David Holstius <david.holstius@gmail.com>
2020-10-20 15:10:30 -0700

    add overlay_raster

---
+++ R/overlay_raster.R
@@ -0,0 +1,58 @@
+#' overlay_raster
+#'
+#' @param input_geodata
+#' @param raster_brick
+#' @param fun
+#' @param progress
+#' @param unit
+#' @param ...
+#' @param verbose
+#'
+#' @importFrom exactextractr exact_extract
+#'
+#' @return
+#' @export
+overlay_raster <- function(
+  input_geodata,
+  raster_brick,
+  fun = "mean",
+  progress = TRUE,
+  unit = NULL,
+  ...,
+  verbose = getOption("verbose", default = TRUE)
+) {
+
+  msg <- function (...) if(isTRUE(verbose)) {
+    message("[overlay_raster] ", ...)
+  }
+
+  extracted_data <-
+    as_tibble(
+      exactextractr::exact_extract(
+        raster_brick,
+        input_geodata,
+        fun = fun,
+        progress = progress,
+        ...)) %>%
+    set_names(
+      names(raster_brick))
+
+  try(detach("package:raster"), silent = TRUE)
+
+  overlaid_geodata <-
+    input_geodata %>%
+    bind_cols(extracted_data) %>%
+    as_tibble() %>%
+    st_as_sf()
+
+  if (isFALSE(is.null(unit))) {
+    overlaid_geodata <-
+      mutate_at(
+        overlaid_geodata,
+        vars(one_of(names(extracted_data))),
+        ~ set_units(., unit, mode = "character"))
+  }
+
+  return(overlaid_geodata)
+
+}
