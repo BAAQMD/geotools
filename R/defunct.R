f7aea4fb43f2d2bda1ef1d910d87fb2f63377433
David Holstius <david.holstius@gmail.com>
2022-01-13 09:35:21 -0800

    make merge() methods defunct

--- R/defunct.R
+++ R/defunct.R
@@ -12,3 +12,56 @@ sp_centroid <- function (spobj, ...) {
   .Defunct(msg = "Try sf::st_centroid() or sp::gCentroid() instead.")
   spobj %>% rgeos::gCentroid(...) %>% coordinates %>% as.vector
 }
+
+#' Merge generic
+#'
+#' @param y data.frame
+#' @param by character
+#' @param \dots further arguments, as in \link{merge} generally (e.g. \code{sort})
+#'
+#' @return an object of the same class as \code{x}
+#'
+#' @export
+merge_SpatialDataFrame <- function (x, y, by, ...) {
+
+  .Defunct("sp::merge")
+
+  if (!identical(row.names(x), row.names(x@data)))
+    warning("row.names(x) and row.names(x@data) are not identical")
+  if (by == "row.names")
+    stop("merge_SpatialDataFrame doesn't support merging by row.names (yet)")
+
+  merged_data <- merge(x@data, y, by = by, ...)
+  i <- merged_data[[by]]
+  row.names(merged_data) <- i
+  merged_geom <- geometry(x)[i, ]
+
+  spobj_constructor <- get(class(x))
+  stopifnot(is.function(spobj_constructor))
+  new_obj <- spobj_constructor(merged_geom, merged_data)
+
+  stopifnot(identical(
+    row.names(new_obj),
+    row.names(merged_geom)))
+
+  return(new_obj)
+
+}
+
+#' @describeIn merge_SpatialDataFrame Merge a SpatialPolygonsDataFrame with a data.frame
+#' @param x a \code{SpatialPolygonsDataFrame} object
+merge_SpatialPolygonsDataFrame <- merge_SpatialDataFrame
+
+setMethod(
+  "merge",
+  c("SpatialPolygonsDataFrame", "data.frame"),
+  merge_SpatialPolygonsDataFrame)
+
+#' @describeIn merge_SpatialDataFrame Merge a SpatialPolygonsDataFrame with a data.frame
+#' @param x a \code{SpatialLinesDataFrame} object
+merge_SpatialLinesDataFrame <- merge_SpatialDataFrame
+
+setMethod(
+  "merge",
+  c("SpatialLinesDataFrame", "data.frame"),
+  merge_SpatialLinesDataFrame)
