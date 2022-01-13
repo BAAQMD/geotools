c83c710eaa13977d54fa01fe2c5bf797dfc9c2f3
David Holstius <david.holstius@gmail.com>
2022-01-13 09:36:30 -0800

    add @importFrom

--- R/pull_fips.R
+++ R/pull_fips.R
@@ -4,11 +4,13 @@
 #'
 #' @param geodata geodata with rownames that are FIPS codes
 #'
+#' @importFrom stringr str_match
+#'
 #' @export
 pull_fips <- function (geodata) {
   FIDs <- row.names(geodata)
   FIPS_pattern <- "([0-9]{2})([0-9]{3})([0-9]{6})"
-  FIPS_parts <- str_match(FIDs, FIPS_pattern)[, -1, drop = FALSE]
+  FIPS_parts <- stringr::str_match(FIDs, FIPS_pattern)[, -1, drop = FALSE]
   colnames(FIPS_parts) <- c("state", "county", "tract")
   return(FIPS_parts)
 }
