ca2eed0ade753104c565c61135b8ce8dba8b8f34
David Holstius <david.holstius@gmail.com>
2022-01-13 09:52:15 -0800

    Delete polygon_areas.R

--- R/polygon_areas.R
+++
@@ -1,9 +0,0 @@
-#' Calculate the area of each polygon in a SpatialPolygons* object
-#'
-#' @param spdf `SpatialPolygons*` object
-#'
-#' @export
-polygon_areas <- function (spdf) {
-  .Defunct("st_area")
-  sapply(spdf@polygons, function(x) x@area / 1e6)
-}
