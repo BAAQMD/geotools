1490e653377b49b49d602cb71a402572830c43c3
David Holstius <david.holstius@gmail.com>
2022-01-13 09:49:06 -0800

    add @importFrom

--- R/set_CRS.R
+++ R/set_CRS.R
@@ -4,19 +4,23 @@
 #'
 #' @param x `sf` or `Spatial*` object
 #' @param new_CRS character string or object created via `CRS()`
+#'
+#' @importFrom sf st_crs
+#' @importFrom sp CRS proj4string
+#'
 #' @note The inventory package provides a few: `WGS84` (lng/lat), `UTM10_NAD83`, `UTM10_NAD27`, and `UTM10_WGS84`. For others, see
 #'
 #' @export
 set_CRS <- function (x, new_CRS) {
 
   if (!inherits(new_CRS, "CRS")) {
-    new_CRS <- CRS(new_CRS)
+    new_CRS <- sp::CRS(new_CRS)
   }
 
   if (inherits(x, "sf")) {
-    st_crs(x) <- new_CRS
+    sf::st_crs(x) <- new_CRS
   } else if (inherits(x, "Spatial")) {
-    proj4string(x) <- new_CRS
+    sp::proj4string(x) <- new_CRS
   } else {
     stop("Don't know how to handle that class of object")
   }
