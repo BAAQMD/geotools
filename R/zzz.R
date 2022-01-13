bc4819d655c9110ff97f14be4f663e48fa98b03a
David Holstius <david.holstius@gmail.com>
2022-01-13 13:11:51 -0800

    no longer issue warning about st_sample()

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,16 @@
 .onLoad <- function(libname, pkgname) {
 
-  st_sample_msg <-
-    stringr::str_c(
-      "-------------------------------------------------------------------------------",
-      "A better `st_sample()` is now provided by the `sf` package.",
-      "Its behavior is different, but more well-supported and future-proofed. ",
-      "If you really want to use the old `geotools::st_sample()`, ",
-      "then please invoke `st_sample_geotools()` instead.",
-      "-------------------------------------------------------------------------------",
-      sep = "\n")
-
-  packageStartupMessage(
-    st_sample_msg)
+  # st_sample_msg <-
+  #   stringr::str_c(
+  #     "-------------------------------------------------------------------------------",
+  #     "A better `st_sample()` is now provided by the `sf` package.",
+  #     "Its behavior is different, but more well-supported and future-proofed. ",
+  #     "If you really want to use the old `geotools::st_sample()`, ",
+  #     "then please invoke `st_sample_geotools()` instead.",
+  #     "-------------------------------------------------------------------------------",
+  #     sep = "\n")
+  #
+  # packageStartupMessage(
+  #   st_sample_msg)
 
 }
