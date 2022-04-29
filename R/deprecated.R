f453e7fd53dd4f11a0339e4724bec646cd5daa02
David Holstius <david.holstius@gmail.com>
2022-04-29 09:14:38 -0700

    downgrade filter_.Spatial*() from deprecated to defunct

--- R/deprecated.R
+++ R/deprecated.R
@@ -27,36 +27,6 @@ mutate.SpatialLinesDataFrame <- mutate_SpatialData
 #' @export
 mutate.SpatialPolygonsDataFrame <- mutate_SpatialData
 
-#' Filter (subset) a Spatial*DataFrame object
-#'
-#' @param .data a \code{Spatial*DataFrame}
-#' @param ... see \link{subset}
-#' @param .dots
-#'
-#' @importFrom dplyr filter_
-#' @importFrom lazyeval all_dots lazy_eval
-#'
-#' @return a subset of the original \code{Spatial*DataFrame}
-#'
-#' @seealso subset
-#'
-#' @export
-filter_.SpatialDataFrame <- function (.data, ..., .dots) {
-  .Deprecated(msg = "upgrade code to use `sf` instead of `sp`")
-  dots <- lazyeval::all_dots(.dots, ..., all_named = TRUE)
-  masks <- lazyeval::lazy_eval(dots, data = as.data.frame(.data@data))
-  subset(.data, Reduce(`&&`, masks))
-}
-
-filter_.SpatialLinesDataFrame <-
-  filter_.SpatialDataFrame
-
-filter_.SpatialPointsDataFrame <-
-  filter_.SpatialDataFrame
-
-filter_.SpatialPolygonsDataFrame <-
-  filter_.SpatialDataFrame
-
 #' fortify_xy
 #'
 #' For use with ggplot2 (plotting polygons)
