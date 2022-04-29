232f3b739bce4f6759076b26a7d50167bdff8a46
David Holstius <david.holstius@gmail.com>
2022-04-29 09:09:03 -0700

    Delete test-gContainsOrOverlaps.R
    - code didn't contain a test

--- tests/testthat/test-gContainsOrOverlaps.R
+++
@@ -1,8 +0,0 @@
-context("gContainsOrOverlaps")
-
-test_that("land area preserved", {
-
-  spobj1 <- filter(CARE::CARE_region_geodata, CARE_name == "Richmond")
-  spobj2 <- filter(TIGER2015::TIGER2015_SFBA_tracts, str_detect(GEOID, "^06013"))
-
-})
