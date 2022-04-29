24e539683cd65a17841ccb47c21f235a90f1a2ac
David Holstius <david.holstius@gmail.com>
2022-04-29 10:13:09 -0700

    run mapviewOptions(fgb = FALSE) on load

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,6 @@
 .onLoad <- function(libname, pkgname) {
 
-  # st_sample_msg <-
-  #   stringr::str_c(
-  #     "-------------------------------------------------------------------------------",
-  #     "A better `st_sample()` is now provided by the `sf` package.",
-  #     "Its behavior is different, but more well-supported and future-proofed. ",
-  #     "If you really want to use the old `geotools::st_sample()`, ",
-  #     "then please invoke `st_sample_geotools()` instead.",
-  #     "-------------------------------------------------------------------------------",
-  #     sep = "\n")
-  #
-  # packageStartupMessage(
-  #   st_sample_msg)
+  message("[geotools] setting mapviewOptions(fgb = FALSE)")
+  mapview::mapviewOptions(fgb = FALSE)
 
 }
