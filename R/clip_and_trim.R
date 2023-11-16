303b9e490794bdd0f28895a8d7fa0a3189b1bae7
David Holstius <david.holstius@gmail.com>
2023-11-16 14:41:37 -0800

    make clip_and_trim() defunct

--- R/clip_and_trim.R
+++
@@ -1,5 +0,0 @@
-clip_and_trim <- function (rst, lower = .Machine$double.eps, upper = Inf) {
-  clamped <- raster::clamp(rst, -Inf, upper, useValues = TRUE)
-  clipped <- raster::clamp(clamped, lower, Inf, useValues = FALSE)
-  raster::trim(clipped)
-}
