4fe90893ee8d71dbd1cf609cc1d6a7b31af6fa22
David Holstius <david.holstius@gmail.com>
2023-11-16 14:41:45 -0800

    make force_crs() defunct

--- R/defunct.R
+++ R/defunct.R
@@ -166,3 +166,57 @@ clip_and_trim <- function (rst, lower = .Machine$double.eps, upper = Inf) {
   raster::trim(clipped)
 }
 
+#' Force replacement of CRS while leaving coordinate values unchanged
+#'
+#' @param geodata `sf` or `sfc` object
+#' @param crs passed to `sf::st_as_sf()``
+#' @param ... further arguments to [sf::st_as_sf()]
+#' @param verbose display messages
+#'
+#' @importFrom sf st_as_sf
+#'
+#' @export
+force_crs <- function (
+    geodata,
+    crs,
+    ...,
+    verbose = getOption("verbose")
+) {
+
+  .Defunct("sf::st_set_crs()")
+
+  msg <- function (...) if(isTRUE(verbose)) message("[force_crs] ", ...)
+
+  # Only used in this function, temporarily.
+  coord_vars <- c(".x_coord", ".y_coord")
+
+  #
+  # These are all helper function from `geotools`:
+  #
+  # - is_geodata()
+  # - coordinates_to_columns()
+  # - drop_geometry()
+  #
+  stopifnot(is_geodata(geodata))
+  fortified <- coordinates_to_columns(geodata, coord_vars)
+  naive <- drop_geometry(fortified)
+
+  # Coerce it back into an `sf` object
+  #
+  # FIXME: not designed to handle the case where `geodata` inherits from `sfc`
+  #
+  if (inherits(geodata, "sfc")) {
+    warning("[force_crs] casting back to `sf`, not `sfc`")
+  }
+
+  coerced <-
+    sf::st_as_sf(
+      naive,
+      coords = coord_vars,
+      crs = crs,
+      ...)
+
+  return(coerced)
+
+}
+
