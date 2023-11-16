be7381e2a9ade80ce3c43f42d7f9155516088794
David Holstius <david.holstius@gmail.com>
2023-11-16 14:10:48 -0800

    clean up datums

--- data/datums.R
+++ data/datums.R
@@ -1,34 +1,25 @@
-#
-# Note: see `geotools-package.R` for associated documentation.
-#
+#'----------------------------------------------------------------------
+#'
+#' See also:
+#'
+#' - `geotools-package.R` for associated documentation.
+#' - `data/CMAQ_CRS.rda` (and `proj4string_LCC()`)
+#'
+#'----------------------------------------------------------------------
 
-NAD83_UTM10 <- 26910
+# WGS 84 / UTM zone 10N, https://epsg.io/32610
+UTM10_CRS   <- sf::st_crs(32610)
 
-UTM10_NAD83 <-
-  sp::CRS("+init=epsg:26910")
+# NAD83 / UTM zone 10N, https://epsg.io/26910
+UTM10_NAD83 <- sf::st_crs(26910)
 
-UTM10_NAD27 <-
-  sp::CRS("+proj=utm +zone=10 +datum=NAD27 +ellps=clrk66 +nadgrids=@conus,@alaska,@ntv2_0.gsb,@ntv1_can.da")
+UTM10_NAD27 <- sf::st_crs("+proj=utm +zone=10 +datum=NAD27 +ellps=clrk66 +nadgrids=@conus,@alaska,@ntv2_0.gsb,@ntv1_can.da")
 
-WGS84_GPS <- 4326
+# Most typical for lon/lat
+WGS84_CRS   <- sf::st_crs(4326)
+WGS84_GPS   <- sf::st_crs(4326)
 
-WGS84 <-
-  sp::CRS("+init=epsg:4326")
+# Web mapping
+WEB_MERCATOR <- sf::st_crs(3857)
 
-EPSG_4326 <-
-  sp::CRS("+init=epsg:4326")
-
-EPSG_3310 <-
-  sp::CRS("+init=epsg:3310")
-
-EPSG_32610 <-
-  sp::CRS("+init=epsg:32610")
-
-UTM10 <-
-  sp::CRS("+init=epsg:32610")
-
-CA_ALBERS_NAD83 <-
-  sp::CRS("+init=epsg:3310")
-
-WEB_MERCATOR <-
-  sf::st_crs(3857)
+CA_ALBERS_NAD83 <- sf::st_crs(3310)
