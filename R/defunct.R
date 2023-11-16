67c37a60eebbd8a88381de55d122a4f022f0b12f
David Holstius <david.holstius@gmail.com>
2023-11-16 14:41:37 -0800

    make clip_and_trim() defunct

--- R/defunct.R
+++ R/defunct.R
@@ -158,3 +158,11 @@ reproject <- function (
   return(reprojected)
 
 }
+
+clip_and_trim <- function (rst, lower = .Machine$double.eps, upper = Inf) {
+  .Defunct()
+  clamped <- raster::clamp(rst, -Inf, upper, useValues = TRUE)
+  clipped <- raster::clamp(clamped, lower, Inf, useValues = FALSE)
+  raster::trim(clipped)
+}
+
