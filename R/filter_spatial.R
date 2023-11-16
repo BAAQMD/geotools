ca17bdd37dc4b31f7e4ae1af0cef33d73867dc9f
David Holstius <david.holstius@gmail.com>
2023-11-16 14:33:22 -0800

    update filter_spatial() to use `sf`

--- R/filter_spatial.R
+++ R/filter_spatial.R
@@ -1,45 +1,51 @@
-#' Apply a spatial filter
+#' filter_spatial
 #'
-#' @param x ([Spatial][sp::Spatial] object) to be filtered
-#' @param y ([Spatial][sp::Spatial] object) to filter with
-#' @param FUN (function) a spatial predicate, for example: [gIntersects][rgeos::gIntersects]
-#' @param ... further arguments to `FUN`
+#' A slightly more convenient version of [sf::st_filter()]. It does the
+#' job of reprojecting the second object to the same CRS as the first,
+#' if that's needed.
+#'
+#' @param x object to be filtered
+#' @param y object to filter with
+#' @param FUN (function) a spatial predicate, for example: [sf::st_intersects()]
+#' @param ... further arguments to [sf::st_filter()]
 #' @param verbose (logical)
 #'
-#' @importFrom sf st_as_sf st_transform st_union st_crs st_within
+#' @importFrom sf st_as_sf st_transform st_filter
 #'
-#' @return (typically) a subset of \code{spobj1}
+#' @return (typically) a subset of \code{x}
 #'
 #' @export
 filter_spatial <- function (
   x,
   y,
-  FUN = NULL,
-  crs = NULL,
+  FUN = st_intersects,
   ...,
   verbose = getOption("verbose")
 ) {
 
   msg <- function (...) if (isTRUE(verbose)) message("[filter_spatial] ", ...)
 
-  if (is.null(FUN)) {
-    FUN <- sf::st_within
+  # if needed, try converting `x` to a spatial object
+  if (!any(class(x) %in% c("sfc"))) {
+    x <- sf::st_as_sf(x)
   }
 
-  if (is.null(crs)) {
-    crs <- sf::st_crs(x)
+  # if needed, try converting `y` to a spatial object
+  if (!any(class(y) %in% c("sfc"))) {
+    y <- sf::st_as_sf(y)
+  }
+
+  # if needed, reproject y to have the same CRS as x
+  if (st_crs(y) != st_crs(x)) {
+    y <- st_transform(y, st_crs(x))
   }
 
   filtered <-
     sf::st_filter(
-      sf::st_as_sf(x),
-      sf::st_transform(sf::st_as_sf(y), crs),
+      x,
+      y,
       .predicate = FUN,
       ...)
-
-  if (inherits(x, "Spatial")) {
-    filtered <- as(filtered, "Spatial")
-  }
 
   return(filtered)
 
--- tests/testthat/test-filter_spatial.R
+++ tests/testthat/test-filter_spatial.R
@@ -3,46 +3,22 @@ context("filter_spatial")
 # See https://cran.r-project.org/web/packages/sp/vignettes/CRS_warnings.html
 options("rgdal_show_exportToProj4_warnings" = "none")
 
-expect_warning(
-  SFBA_tracts_WGS84 <-
-    TIGER2015::TIGER2015_SFBA_tracts %>%
-    reproject(
-      new_CRS = WGS84_CRS),
-  "is deprecated")
-
-#
-# Tests with `ALA_county_WGS84`
-#
-expect_warning(
-  ALA_county_WGS84 <-
-    TIGER2015::TIGER2015_CA_counties %>%
-    subset(
-      str_detect(.$GEOID, "^06001")) %>%
-    reproject(
-      new_CRS = WGS84_CRS),
-  "is deprecated")
-
-test_that("Alameda County (sp)", {
+test_that("San Francisco", {
 
   # Simple test: only containment (shouldn't be any "overlapping")
+
+  filter_region <-
+    SFBA::SFBA_county_TIGER_2010_geometry |>
+    filter(county_id == "06075") |> # San Francisco
+    st_transform(UTM10_CRS) |>
+    st_buffer(-100)
 
   test_tracts <-
     filter_spatial(
-      SFBA_tracts_WGS84,
-      ALA_county_WGS84)
+      SFBA::SFBA_tract_TIGER_2010_geometry,
+      filter_region)
 
   expect_equal(
-    nrow(test_tracts), 361)
-
-})
-
-test_that("Alameda County (sf)", {
-
-  # Simple test: only containment (shouldn't be any "overlapping")
-
-  sf1 <- st_as_sf(SFBA_tracts_WGS84)
-  sf2 <- st_as_sf(ALA_county_WGS84)
-  test_tracts <- filter_spatial(sf1, sf2)
-  expect_equal(nrow(test_tracts), 361)
+    nrow(test_tracts), 197)
 
 })
